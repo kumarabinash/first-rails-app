1e0418195a7be23d8acb905fe4ddc88b3f9f17c9
Kumar Abinash <abinash.b2@gmail.com>
2013-08-16 05:17:32 +0530

    Testing the first git commit

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,10 @@
+class SayController < ApplicationController
+  def index
+  end
+
+  def hello
+  end
+
+  def goodbye
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = 'ae615a58b3722fa35e9b5ea880817e3e9035c5667b91f74a475c83dd3858fee32b24b5d602247fe9a5b9aaf6d001cbe6757656b48b29b2ae41ef32d84948e638'
